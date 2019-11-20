6a8a2e93fd588385d80ebb26e89d50b98d585bff
vladhiriscau <vlad.hiriscau@yahoo.com>
2019-11-20 20:30:23 +0200

    added modulo exemples

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -76,6 +76,9 @@ public class App
 //        Car car4 = null;
 //        System.out.println(car4.name);
 
-
+//        System.out.println("Modulo example:");
+//        System.out.println(4 % 2);
+//        System.out.println(4 % 3);
+//        System.out.println(4 % 2 == 0);
 }
     }
