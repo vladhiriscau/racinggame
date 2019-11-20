135fa1b2ae2c82bf20f0524090764075393692a8
vladhiriscau <vlad.hiriscau@yahoo.com>
2019-11-20 20:44:49 +0200

    added comment for local variable

--- src/main/java/org/fasttrackit/Vehicle.java
+++ src/main/java/org/fasttrackit/Vehicle.java
@@ -14,6 +14,7 @@ public class Vehicle {
     public double accelerate(double speed, double durationInHours) {
         System.out.println(name + "is accelerating with: " + speed + " for " + durationInHours + "h.");
 
+        //local variable (declared inside a method)
         double distance = speed * durationInHours;
 
         traveledDistance = traveledDistance + distance;
