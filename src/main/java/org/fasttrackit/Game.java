94f27bf24c28ac03f6e2dc78a5c483ecd565fd70
vladhiriscau <vlad.hiriscau@yahoo.com>
2019-12-04 21:16:28 +0200

    studied arrays classic for loop

--- src/main/java/org/fasttrackit/Game.java
+++ src/main/java/org/fasttrackit/Game.java
@@ -6,14 +6,51 @@ import java.util.Scanner;
 
 public class Game {
 
+    private  Track[] tracks = new Track[3];
+
+
     public void  start() {
         System.out.println("Starting game...");
+
+        initializeTracks();
+        displayTracks();
 
         int playerCount = getPlayerCountFromUser();
         System.out.println("Number of players:" + playerCount);
 
         String vehicleName = getVehicleNameFromUser();
         System.out.println("Name from user:" + vehicleName);
+
+    }
+
+    private void  initializeTracks() {
+        Track track1 = new Track();
+        track1.setName("Silverstone");
+        track1.setLenght(4.2);
+
+        tracks[0] = track1;
+
+        Track track2 = new Track();
+        track2.setName("Transylvania");
+        track2.setLenght(400.2);
+
+        tracks[1] = track2;
+
+        System.out.println(tracks[0].getName());
+
+
+    }
+
+    private  void  displayTracks() {
+        System.out.println("Available tracks:");
+        System.out.println(tracks);
+
+        // classic for loop
+        for (int i = 0; i < tracks.length; i++) {
+            if (tracks[i] != null) {
+                System.out.println((i + 1) + ". " + tracks[i].getName() + " - " + tracks[i].getLenght() + "km");
+            }
+        }
 
     }
 
---
+++ src/main/java/org/fasttrackit/Track.java
@@ -0,0 +1,31 @@
+package org.fasttrackit;
+
+public class Track {
+
+    private String name;
+    private double lenght;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getLenght() {
+        return lenght;
+    }
+
+    public void setLenght(double lenght) {
+        this.lenght = lenght;
+    }
+
+    @Override
+    public String toString() {
+        return "Track{" +
+                "name='" + name + '\'' +
+                ", lenght=" + lenght +
+                '}';
+    }
+}
